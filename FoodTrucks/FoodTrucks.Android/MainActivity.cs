6a5d7ef390351975eb546c2c702378d34646b112
pratik8490 <p.kankhara@gmail.com>
2016-03-06 01:55:43 +0530

    Initial commit
    solution of project.

---
+++ FoodTrucks/FoodTrucks.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+namespace FoodTrucks.Droid
+{
+    [Activity(Label = "FoodTrucks", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            global::Xamarin.FormsMaps.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ FoodTrucks/FoodTrucks/App.cs
@@ -0,0 +1,44 @@
+﻿using FoodTrucks.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FoodTrucks
+{
+    public class App : Application
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            // The root page of your application 
+            MainPage = new MapPage();
+        }
+        #endregion
+
+        static NavigationPage navPage;
+        public static Color BarTextColor()
+        {
+            return Color.White;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts 
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps 
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes 
+        }
+    }
+}
