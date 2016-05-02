4c2e5f9ceaed4384353f9f4d06680f4fe7ef745f
pratik8490 <p.kankhara@gmail.com>
2016-05-02 23:32:48 +0530

    mising files

---
+++ FoodTrucks.Provider/FoodTypeProvider.cs
@@ -0,0 +1,58 @@
+﻿using FoodTrucks.Provider.Interface;
+using FoodTrucks.Provider.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FoodTrucks.Provider
+{
+    public class FoodTypeProvider : IFoodType
+    {
+        private string _SiteUrl = string.Empty;
+
+        public FoodTypeProvider()
+        {
+            _SiteUrl = "http://foodlifttrucks.com/";
+        }
+        public Task<FoodTypeModel> GetFoodTypeByID(int id)
+        {
+            var foodType = new TaskCompletionSource<FoodTypeModel>();
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                try
+                {
+
+                    HttpClient client = new HttpClient();
+                    client.BaseAddress = new Uri(_SiteUrl);
+
+                    var requestUri = "api/FoodType?id=" + id;
+
+                    var response = await client.GetAsync(requestUri);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        foodType.TrySetResult(JsonConvert.DeserializeObject<FoodTypeModel>(content));
+                    }
+                    else
+                    {
+                        foodType.SetResult(new FoodTypeModel());
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            });
+
+            return foodType.Task;
+        }
+    }
+}
